192c78237a7a005cb896e89185be4e48c5c897eb
Jacob85 <yaki.ams@gmail.com>
2014-05-11 23:01:35 +0300

    Added log4J Sample

---
+++ src/il/ac/shenkar/log4j/Log4JDemo.java
@@ -0,0 +1,39 @@
+package il.ac.shenkar.log4j;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Jacob
+ * Date: 5/11/14
+ * Time: 10:38 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Log4JDemo
+{
+    private static Logger logger = Logger.getLogger(Log4JDemo.class);
+
+    public static void main(String args[])
+    {
+        PropertyConfigurator.configure("C:\\Users\\Jacob\\IdeaProjects\\JavaCodeSamples\\conf\\log4j.properties");
+        logger.info("This is a demo info message");
+
+        //this code is for creating a null pointer Exception
+        try
+        {
+            String nullString = null;
+            nullString.toCharArray();
+        }
+        catch (NullPointerException e)
+        {
+            logger.error("Something went wrong (" + e.getMessage() + ")...", e);
+        }
+
+        for (int i =0; i < 10 ; i++)
+        {
+            logger.debug("i now is: " + i);
+        }
+
+    }
+}
